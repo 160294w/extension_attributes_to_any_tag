b8ee4863c0f223d4102ba0911f80fa6685ea7a92
160294w <160294w@gmail.com>
2023-05-06 01:15:11 +0900

    Add: complete implementation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # extension_attributes_to_any_tag
 任意のタグに任意の属性を追加するChrome拡張機能
+
+## Usage
+Tag Name: 属性を追加したいタグ
+Attribute Name: 追加したい属性名
+Attribute value: 追加したい値
+
+![](./image.png)
--- popup.js
+++ popup.js
@@ -1,17 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const addAttributeButton = document.getElementById('add-attribute');
-  addAttributeButton.addEventListener('click', function() {
+
+//function injectedCodes(tag, attribute, value) {
+// async function getCurrentTab() {
+//     let queryOptions = { active: true, lastFocusedWindow: true };
+//     // `tab` will either be a `tabs.Tab` instance or `undefined`.
+//     let [tab] = await chrome.tabs.query(queryOptions);
+//     return tab[0];
+
+// }
+
+function setAttr(tag, attribute, value) {
+    console.log("call setAttr func");
+    console.log(tag)
+    console.log(attribute)
+    console.log(value)
+    // var elements = document.getElementsByTagName('${tag}');
+    // for (var i = 0; i < elements.length; i++) {
+    //     elements[i].setAttribute('${attribute}', '${value}');
+    // }
+    var elements = document.getElementsByTagName(tag);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].setAttribute(attribute, value);
+    }
+    
+    console.log("call setAttr func");
+}
+//}
+
+
+const addAttributeButton = document.getElementById('add-attribute');
+addAttributeButton.addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      const tag = document.getElementById('tag').value;
-      const attribute = document.getElementById('attribute').value;
-      const value = document.getElementById('value').value;
-      const code = `
-        var elements = document.getElementsByTagName('${tag}');
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].setAttribute('${attribute}', '${value}');
-        }
-      `;
-      chrome.tabs.executeScript(tabs[0].id, { code: code });
+	const tag = document.getElementById('tag').value;
+	const attribute = document.getElementById('attribute').value;
+	const value = document.getElementById('value').value;
+	chrome.scripting.executeScript({
+	    //target: {tabId: getTabId()},
+	    target: {tabId: tabs[0].id },//getCurrentTab().id}, 
+	    func: setAttr,
+	    args: [tag, attribute, value ]
+	});
     });
-  });
 });
